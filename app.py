5c3aa30245e1cc92a51eeaf092ed5d7ba53de09c
Elango <elangoraj651@gmail.com>
2023-08-14 14:29:26 +0530

    production level

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def upload():
 
 
 if __name__ == '__main__':
-    app.run(port=8501, debug=True)
+    app.run(port=8000, debug=False)
 
     # Serve the app with gevent
     # http_server = WSGIServer(('', 5000), app)
