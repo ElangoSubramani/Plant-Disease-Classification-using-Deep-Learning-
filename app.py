8134dceb46d2cc4367e1abfb796ff505721a3317
Elango <elangoraj651@gmail.com>
2023-08-14 15:00:14 +0530

    hi

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ def upload():
 if __name__ == '__main__':
     app.run(port=8000, debug=False)
 
+
     # Serve the app with gevent
     # http_server = WSGIServer(('', 5000), app)
     # http_server.serve_forever()
